e1eac1b6c1c3dcaa5c8af0f6bea612e2f934f820
Paranjay Soni <soniparanjay@gmail.com>
2026-04-06 14:41:11 +0530

    LLM implemented

---
+++ src/langgraph/LLMS/groqllm.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+groq_api_key = st.secrets["GROQ_API_KEY"]
+
+
+class GroqLLM(ChatGroq):
+    def __init__(self, user_control_input):
+        self.user_control_input = user_control_input
+
+    def get_llm_model(self):
+        try:
+            selected_groq_model = self.user_control_input.get("selected_groq_model")
+
+            llm = ChatGroq(
+                api_key=groq_api_key,
+                model=selected_groq_model,
+            )
+        except Exception as e:
+            raise ValueError(f"Error initializing GroqLLM: {e}")
+        
+        return llm
