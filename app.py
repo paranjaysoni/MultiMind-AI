6851b834c4dab8b28df9e218b190507e011d3a13
Paranjay Soni <soniparanjay@gmail.com>
2026-04-06 22:02:16 +0530

    Added Footer Credit

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ groq_api_key = st.secrets["GROQ_API_KEY"]
 
 if __name__ == "__main__":
     load_langgraph_agenticai_app()
+
+# Footer Credit
+st.markdown("---")
+st.markdown(
+    "<h5 style='text-align: center; color: grey;'>Built with  ❤️  by <a href='https://github.com/paranjaysoni' target='_blank' style='color: grey; text-decoration: none;'>Paranjay Soni</a></h5>",
+    unsafe_allow_html=True
+)
